0adf7d0b9af5d76f51116189ed0fd8dc9b5a33cc
Taylor Wagstaff <taylorwagstaff@gmail.com>
2023-09-15 10:37:53 +1200

    timer:

--- js/scripts.js
+++ js/scripts.js
@@ -1,212 +1,249 @@
-$(document).ready(function () {
-  var score = 0
-  var timeLeft = 20
-  var gameStarted = false
-  var timerInterval
-
-  function startGame() {
-    if (!gameStarted) {
-      gameStarted = true
-      $('#shape').show()
-      $('#start-button').prop('disabled', true)
-      $('#stop-button').prop('disabled', true)
-      $('#score-value').text(score)
-      $('#time-left').text(timeLeft)
-      moveShape()
-      updateTimer()
-    }
-  }
-
-  function moveShape() {
-    var maxX = $(window).width() - $('#shape').width()
-    var maxY = $(window).height() - $('#shape').height()
-
-    var newX = Math.random() * maxX
-    var newY = Math.random() * maxY
-
-    $('#shape').css({
-      left: newX + 'px',
-      top: newY + 'px',
-    })
-
-    $('#shape').one('click', function () {
-      if (gameStarted) {
-        score++
-        $('#score-value').text(score)
-        moveShape()
-      }
-    })
-  }
-
-  function updateTimer() {
-    timerInterval = setInterval(function () {
-      timeLeft--
-      $('#time-left').text(timeLeft)
-      if (timeLeft <= 0) {
-        clearInterval(timerInterval)
-        endGame()
-      }
-    }, 1000)
-  }
-
-  function endGame() {
-    gameStarted = false
-    $('#shape').hide()
-    $('#start-button').prop('disabled', false)
-    $('#stop-button').prop('disabled', true)
-    $('#pause-button').prop('disabled', false)
-
-    alert('Game is Over! your score is:' + score)
-    score = 0
-    timeLeft = 20
-    $('#score-value').text(score)
-    $('#time-left').text(timeLeft)
-  }
-  $('#shape').hide()
-  $('#start-button').click(startGame)
-  $('#stop-button').click(endGame)
-  $('#pause-button').click(function () {
-    if (!gameStarted) {
-      startGame();
-    }
-  })
-})
-
-//
-
-//
-
-$(document).ready(function () {
-  var slideIndex = 0
-  var slides = $('.slide')
-  var totalSlides = slides.length
-  var interval = 3000
-
-  function showSlide(index) {
-    slides.eq(index).show()
-  }
-
-  function hideSlide() {
-    slides.hide()
-  }
-
-  function nextSlide() {
-    hideSlide()
-    slideIndex = (slideIndex + 1) % totalSlides
-    showSlide(slideIndex)
-  }
-  function prevSlide() {
-    hideSlide()
-    slideIndex = slideIndex - 1 + totalSlides
-    showSlide(slideIndex)
-  }
-  $('.slider').hover(
-    function () {
-      clearInterval(sliderInterval)
-    },
-    function () {
-      sliderInterval = setInterval(nextSlide, interval)
-    }
+function updateCountdown() {
+  const targetDate = new Date('2023-09-15 10:30:00').getTime()
+  const currentDate = new Date().getTime()
+  const timeDifference = targetDate - currentDate
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   )
-  $('.next').click(nextSlide)
-  $('.prev').click(prevSlide)
-  showSlide(slideIndex)
-  var sliderInterval = setInterval(nextSlide, interval)
-  $(window).resize(function () {
-    hideSlide()
-    showSlide(slideIndex)
-  })
-})
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+  $('#days').text(String(days).padStart(2, '0'))
+  $('#hours').text(String(hours).padStart(2, '0'))
+  $('#minutes').text(String(minutes).padStart(2, '0'))
+  $('#seconds').text(String(seconds).padStart(2, '0'))
+}
 
 $(document).ready(function () {
-  $('#myForm').submit(function (event) {
-    var valid = true
-
-    var name = $('#name').val()
-    var email = $('#email').val()
-    var message = $('#message').val()
-    var phonenumber = $('#phonenumber').val()
-
-    if (name === '') {
-      $('#nameError').text('Name is missing')
-      valid = false
-    } else {
-      $('#nameError').text('')
-    }
-
-    if (email === '') {
-      $('#emailError').text('Email is missing')
-      valid = false
-    } else if (!isValidEmail(email)) {
-      $('emailError').text('invalid Format')
-      valid = 'false'
-    } else {
-      $('#emailError').text('')
-    }
-
-    if (message === '') {
-      $('#messageError').text('message is missing')
-      valid = false
-    } else if (!isValidMessage(message)) {
-      $('messageError').text('invalid Format')
-      valid = 'false'
-    } else {
-      $('#messageError').text('')
-    }
-
-    if (phonenumber === '') {
-      $('#phonenumberError').text('Phone Number is missing')
-      valid = false
-    } else if (!isValidPhoneNumber(phonenumber)) {
-      $('phonenumberError').text('invalid Format')
-      valid = 'false'
-    } else {
-      $('#phonenumberError').text('')
-    }
-
-    if (!valid) {
-      event.preventDefault()
-    }
-  })
-
-  function isValidEmail(email) {
-    var emailPattern = /^[a-zA-z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,4}$/
-    return emailPattern.test(email)
-  }
-
-  function isValidMessage(message) {
-    // must be between four and 196 characters
-    var messagePattern =
-      /'\,\-\!\@\#\$\&\*\`\~\[\]\?\<\>\"\:\;\\\/\{\}\|\%\^\(\)\+\=]{4,196}/
-    return messagePattern.test(message)
-  }
-
-  function isValidPhoneNumber(phonenumber) {
-    var phoneNumberPattern =
-      /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/
-    return phoneNumberPattern.test(phonenumber)
-  }
+  updateCountdown()
+  setInterval(updateCountdown, 1000)
 })
 
-$(document).ready(function () {
-  $('#button').click(function () {
-    var toAdd = $('input[name=ListItem]').val()
-    if (toAdd.trim() !== '') {
-      $('ul').append(
-        '<li>' + toAdd + ' <button class="remove-me">-</button></li>'
-      )
-    }
-  })
+// $(document).ready(function () {
+//   setInterval(function time() {
+//     var d = new Date()
+//     var hours = 24 - d.getHours()
+//     var min = 60 - d.getMinutes()
+//     if ((min + '').length == 1) {
+//       min = '0' + min
+//     }
+//     var sec = 60 - d.getSeconds()
+//     if ((sec + '').length == 1) {
+//       sec = '0' + sec
+//     }
+//     $('#the-final-countdown p').html(hours + ':' + min + ':' + sec)
+//   }, 1000)
+// })
 
-  $('input[name=ListItem]').keyup(function (event) {
-    if (event.keyCode == 13) {
-      $('#button').click()
-    }
-  })
+// $(document).ready(function () {
+//   var score = 0
+//   var timeLeft = 20
+//   var gameStarted = false
+//   var timerInterval
 
-  $('ul').on('click', '.remove-me', function () {
-    $(this).parent().remove()
-  })
+//   function startGame() {
+//     if (!gameStarted) {
+//       gameStarted = true
+//       $('#shape').show()
+//       $('#start-button').prop('disabled', true)
+//       $('#stop-button').prop('disabled', true)
+//       $('#score-value').text(score)
+//       $('#time-left').text(timeLeft)
+//       moveShape()
+//       updateTimer()
+//     }
+//   }
 
-  $('ul').sortable()
-})
+//   function moveShape() {
+//     var maxX = $(window).width() - $('#shape').width()
+//     var maxY = $(window).height() - $('#shape').height()
+
+//     var newX = Math.random() * maxX
+//     var newY = Math.random() * maxY
+
+//     $('#shape').css({
+//       left: newX + 'px',
+//       top: newY + 'px',
+//     })
+
+//     $('#shape').one('click', function () {
+//       if (gameStarted) {
+//         score++
+//         $('#score-value').text(score)
+//         moveShape()
+//       }
+//     })
+//   }
+
+//   function updateTimer() {
+//     timerInterval = setInterval(function () {
+//       timeLeft--
+//       $('#time-left').text(timeLeft)
+//       if (timeLeft <= 0) {
+//         clearInterval(timerInterval)
+//         endGame()
+//       }
+//     }, 1000)
+//   }
+
+//   function endGame() {
+//     gameStarted = false
+//     $('#shape').hide()
+//     $('#start-button').prop('disabled', false)
+//     $('#stop-button').prop('disabled', true)
+//     $('#pause-button').prop('disabled', false)
+
+//     alert('Game is Over! your score is:' + score)
+//     score = 0
+//     timeLeft = 20
+//     $('#score-value').text(score)
+//     $('#time-left').text(timeLeft)
+//   }
+//   $('#shape').hide()
+//   $('#start-button').click(startGame)
+//   $('#stop-button').click(endGame)
+//   $('#pause-button').click(function () {
+//     if (!gameStarted) {
+//       startGame()
+//     }
+//   })
+// })
+
+// //
+
+// //
+
+// $(document).ready(function () {
+//   var slideIndex = 0
+//   var slides = $('.slide')
+//   var totalSlides = slides.length
+//   var interval = 3000
+
+//   function showSlide(index) {
+//     slides.eq(index).show()
+//   }
+
+//   function hideSlide() {
+//     slides.hide()
+//   }
+
+//   function nextSlide() {
+//     hideSlide()
+//     slideIndex = (slideIndex + 1) % totalSlides
+//     showSlide(slideIndex)
+//   }
+//   function prevSlide() {
+//     hideSlide()
+//     slideIndex = slideIndex - 1 + totalSlides
+//     showSlide(slideIndex)
+//   }
+//   $('.slider').hover(
+//     function () {
+//       clearInterval(sliderInterval)
+//     },
+//     function () {
+//       sliderInterval = setInterval(nextSlide, interval)
+//     }
+//   )
+//   $('.next').click(nextSlide)
+//   $('.prev').click(prevSlide)
+//   showSlide(slideIndex)
+//   var sliderInterval = setInterval(nextSlide, interval)
+//   $(window).resize(function () {
+//     hideSlide()
+//     showSlide(slideIndex)
+//   })
+// })
+
+// $(document).ready(function () {
+//   $('#myForm').submit(function (event) {
+//     var valid = true
+
+//     var name = $('#name').val()
+//     var email = $('#email').val()
+//     var message = $('#message').val()
+//     var phonenumber = $('#phonenumber').val()
+
+//     if (name === '') {
+//       $('#nameError').text('Name is missing')
+//       valid = false
+//     } else {
+//       $('#nameError').text('')
+//     }
+
+//     if (email === '') {
+//       $('#emailError').text('Email is missing')
+//       valid = false
+//     } else if (!isValidEmail(email)) {
+//       $('emailError').text('invalid Format')
+//       valid = 'false'
+//     } else {
+//       $('#emailError').text('')
+//     }
+
+//     if (message === '') {
+//       $('#messageError').text('message is missing')
+//       valid = false
+//     } else if (!isValidMessage(message)) {
+//       $('messageError').text('invalid Format')
+//       valid = 'false'
+//     } else {
+//       $('#messageError').text('')
+//     }
+
+//     if (phonenumber === '') {
+//       $('#phonenumberError').text('Phone Number is missing')
+//       valid = false
+//     } else if (!isValidPhoneNumber(phonenumber)) {
+//       $('phonenumberError').text('invalid Format')
+//       valid = 'false'
+//     } else {
+//       $('#phonenumberError').text('')
+//     }
+
+//     if (!valid) {
+//       event.preventDefault()
+//     }
+//   })
+
+//   function isValidEmail(email) {
+//     var emailPattern = /^[a-zA-z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,4}$/
+//     return emailPattern.test(email)
+//   }
+
+//   function isValidMessage(message) {
+//     // must be between four and 196 characters
+//     var messagePattern =
+//       /'\,\-\!\@\#\$\&\*\`\~\[\]\?\<\>\"\:\;\\\/\{\}\|\%\^\(\)\+\=]{4,196}/
+//     return messagePattern.test(message)
+//   }
+
+//   function isValidPhoneNumber(phonenumber) {
+//     var phoneNumberPattern =
+//       /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/
+//     return phoneNumberPattern.test(phonenumber)
+//   }
+// })
+
+// $(document).ready(function () {
+//   $('#button').click(function () {
+//     var toAdd = $('input[name=ListItem]').val()
+//     if (toAdd.trim() !== '') {
+//       $('ul').append(
+//         '<li>' + toAdd + ' <button class="remove-me">-</button></li>'
+//       )
+//     }
+//   })
+
+//   $('input[name=ListItem]').keyup(function (event) {
+//     if (event.keyCode == 13) {
+//       $('#button').click()
+//     }
+//   })
+
+//   $('ul').on('click', '.remove-me', function () {
+//     $(this).parent().remove()
+//   })
+
+//   $('ul').sortable()
+// })
